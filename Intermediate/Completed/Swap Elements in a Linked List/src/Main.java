ec024fc4f73fd83906b532aa4a97ea989ed3e7c5
herikerbeth <herik.docarmo@hotmail.com>
2023-07-06 16:18:37 -0300

    started second challenge

---
+++ Intermediate/Completed/Swap Elements in a Linked List/src/Main.java
@@ -0,0 +1,20 @@
+/*
+Given the head of a linked list and two values within the list, create a swapNodes() method
+that swaps the nodes with the given values. Your function should return the same list with the
+nodes swapped.
+For example, if your original linked list was named demoList and contained 1 → 2 → 3 → 4
+→ 5 → 6, swapNodes(demoList, 2, 5) should return the list containing 1 → 5 → 3 → 4 →
+2 → 6.
+*/
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> demoList = new LinkedList<>();
+        demoList.addAll(List.of(1, 2, 3, 4, 5));
+
+        
+    }
+}
